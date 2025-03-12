edea1dd77fae1a067ff1adb7d4ca2a3b97fe2974
Jake Bowhay <jb9.bowhay@gmail.com>
2025-03-12 15:41:37 +0000

    begin prototyping subdivision

--- src/scikit_poles_zeros/_domain.py
+++ src/scikit_poles_zeros/_domain.py
@@ -1,13 +1,43 @@
-from __future__ import annotations
+from collections import deque
+from functools import cached_property
 
 
 class Rectangle:
-    """Rectangle region in the complex plane"""
+    """Rectangle region in the complex plane."""
 
     def __init__(self, bottom_left, top_right, /):
         self.bottom_left = bottom_left
         self.top_right = top_right
 
         # children created if region is subdivided
-        self.child_a = None  # left/top region
-        self.child_b = None  # right/bottom region
+        # 0th entry left/top, 1st entry right/bottom
+        self.children = None
+
+    @cached_property
+    def corners(self):
+        """Returns the corners of the rectangle in a counter clockwise order
+        starting from the bottom left."""
+        bl = self.bottom_left
+        tr = self.top_right
+        return (bl, tr.real + bl.imag, tr, bl.real + tr.imag)
+
+    def contour_integral(self, _):
+        """Compute the contour integral of `f` around the region."""
+
+    def subdivide(self): ...
+
+
+def _subdivide_domain(domain, f, f_z, f_zz, max_poles_zeros):
+    queue = deque(domain)
+    while len(queue) > 0:
+        current_domain = queue.popleft()
+
+        # 1. Compute the combined number of poles and zeros in the domain
+        def second_log_derivative(z):
+            return (f_zz(z) / f(z)) - (f_z(z) / f(z)) ** 2
+
+        n_poles_zeros = current_domain.contour_integral(second_log_derivative)
+        # 2. Subdivide and repeat if this number is too high
+        if n_poles_zeros > max_poles_zeros:
+            current_domain.subdivide()
+            queue.append(current_domain.children)
