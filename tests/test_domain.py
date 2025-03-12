5a08aefd9b4720c473ab1677f4e426d077c3c6c3
Jake Bowhay <jb9.bowhay@gmail.com>
2025-03-12 18:53:23 +0000

    rename test file

--- tests/test_domain.py
+++ tests/test_domain.py
@@ -1,10 +1,8 @@
-from __future__ import annotations
-
 from scikit_poles_zeros._domain import Rectangle
 
 
 class TestRectangle:
-    def test_init(self):
+    def test_attributes(self):
         bl, tr = 0, 10 + 10j
         r = Rectangle(bl, tr)
         assert r.bottom_left == bl
