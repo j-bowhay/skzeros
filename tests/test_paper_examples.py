2a67fc61cd48c2569fdc1336ecf59d002d36d257
Jake Bowhay <jb9.bowhay@gmail.com>
2025-07-01 15:55:44 +0100

    add test for example 1

--- tests/test_paper_examples.py
+++ tests/test_paper_examples.py
@@ -1,7 +1,10 @@
 # test that ensure the examples given in the paper are working correctly
 import numpy as np
+import pytest
+from numpy.testing import assert_allclose
+from scipy.stats import qmc
 
-from skzeros import AAA, Rectangle, poles_residues
+from skzeros import AAA, Rectangle, find_zeros, poles_residues
 
 
 def test_branch_cut():
@@ -30,3 +33,23 @@ def test_branch_cut():
     mask_2 = (np.abs(poles_2.real) < 5) & (np.abs(poles_2.imag) < 5)
     assert np.sum(np.isclose(residue_1[mask_1], 1, atol=1e-2, rtol=0)) == 2
     assert np.sum(np.isclose(residue_2[mask_2], 1, atol=1e-2, rtol=0)) == 8
+
+
+@pytest.mark.filterwarnings("ignore:.*balance properties of Sobol' points require*")
+def test_example_1():
+    N = 100
+    rng = np.random.default_rng(12345)
+    sampler = qmc.Sobol(d=2, rng=rng)
+    tmp = sampler.random(N)
+    zeros = tmp[:, 0] + tmp[:, 1] * 1j
+
+    def f(z):
+        return np.prod(np.subtract.outer(z, zeros), axis=-1)
+
+    def f_z(z):
+        tmp = np.subtract.outer(z, zeros)
+        return np.prod(tmp, axis=-1) * np.sum(1 / tmp, axis=-1)
+
+    d = Rectangle(0, complex(1, 1))
+    res = find_zeros(d, f, f_z, max_arg_principle=7.1)
+    assert_allclose(np.sort(res.zeros), np.sort(zeros))
